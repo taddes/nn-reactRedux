a1eb0b034e247b299d5ecad078141e853819d2f0
Taddes Korris <taddes.korris@passportinc.com>
2019-02-07 14:05:13 -0500

    react app

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Crew from './Crew';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Heyo!</h1>  
+        <Crew />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ myapp/src/Crew.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Crew extends Component {
+  render() {
+    return (
+      <div className="crew">
+        <div>Name: James Kirk</div>
+        <div>Rank: Captain</div>
+        <div>Race: Human</div>
+      </div>
+
+
+
+
+    )
+  }
+}
+
+export default Crew
