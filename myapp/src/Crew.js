ab0dec313cf895c5f66cfb49e62b375fb8ea0401
Taddes Korris <taddes.korris@passportinc.com>
2019-02-10 12:26:29 -0500

    types of components

--- myapp/src/Crew.js
+++ myapp/src/Crew.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Crew extends Component {
-  render() {
-    console.log(this.props)
-    const {crew} = this.props;
-    const crewList = crew.map(crewMember => {
-      return(
-        <div className="crew" key={crewMember.id}>
-        <div>Name: {crewMember.name }</div>
-        <div>Rank: {crewMember.rank}</div>
-        <div>Race: {crewMember.race}</div>
-      </div>
-      )
-    })
+const Crew = (props) => {
+// const Crew = ({crew}) => {
+  const {crew} = props;
+  const crewList = crew.map(crewMember => {
+    return(
+    <div className="crew" key={crewMember.id}>
+      <div>Name: {crewMember.name }</div>
+      <div>Rank: {crewMember.rank}</div>
+      <div>Race: {crewMember.race}</div>
+      <br />
+    </div>
+    )
+  })
     return (
       <div className="crew-list">
       { crewList }
       </div>
     )
   }
-}
 
 export default Crew
