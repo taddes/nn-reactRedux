27d09408e56a8841d9c33715bba99c070d784365
Taddes Korris <taddes.korris@passportinc.com>
2019-05-20 17:14:34 -0400

    test apps

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return(
+    <div className="container">
+      <h4 className="center">Contactpage</h4>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia aspernatur, odit reiciendis, quas et ducimus quidem consequatur omnis quos eveniet dolores alias a, expedita recusandae explicabo? Possimus provident sequi temporibus.</p>
+    </div>
+  )
+}
+
+export default Contact;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return(
+    <div className="container">
+      <h4 className="center">Homepage</h4>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia aspernatur, odit reiciendis, quas et ducimus quidem consequatur omnis quos eveniet dolores alias a, expedita recusandae explicabo? Possimus provident sequi temporibus.</p>
+    </div>
+  )
+}
+
+export default Home;
