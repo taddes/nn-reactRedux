dfe2cb75e03dcc494af775c28effbf275524fea0
Taddes Korris <taddes.korris@passportinc.com>
2019-05-21 17:11:51 -0400

    udpates

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    setTimeout(() => {
+    props.history.push('/about')
+  }, 2000)
   return(
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poketimes</a>
+        <a className="brand-logo" href="/">Poketimes</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
 
