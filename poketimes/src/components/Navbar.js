a28d16d91bb30df0d4d9dc19ca4565ebc73d44b4
Taddes Korris <taddes.korris@passportinc.com>
2019-05-21 17:14:01 -0400

    udpates

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = (props) => {
-    setTimeout(() => {
-    props.history.push('/about')
-  }, 2000)
+const Navbar = () => {
+
   return(
     <nav className="nav-wrapper red darken-3">
       <div className="container">
@@ -20,4 +18,4 @@ const Navbar = (props) => {
   )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
