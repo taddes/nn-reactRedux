7887a3f7f5863ef6a3782048d12f0177f28471f2
Taddes Korris <taddes.korris@passportinc.com>
2019-05-24 13:08:31 -0400

    completed actions and reducer for redux

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,8 +1,21 @@
 const initState = {
-  posts: []
+  posts: [
+    {id: 1, title: 'Eat Cake', body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'},
+    {id: 2, title: 'Harmless Flies', body: 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla'},
+    {id: 3, title: 'Vindicated Puppets', body: 'ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit'}
+  ]
 }
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST') {
+    let newPosts = state.posts.filter(post => {
+      return action.id !== post.id
+  });
+    return {
+      ...state,
+      posts: newPosts
+    }
+  } 
   return state;
 }
 
